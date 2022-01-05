cdb28fb610180bf29c82ea18c39ed0ea5f103582
Atul singh <Atul singh@Atul>
2022-01-05 23:41:47 +0530

    First Commit

---
+++ src/com/hashTable/MyNode.java
@@ -0,0 +1,25 @@
+package com.hashTable;
+public class MyNode <K>implements INode<K>{
+    private  K Key;
+    private  INode next;
+    public  MyNode(K key){
+        this.Key = key;
+        this.next = null;
+    }
+
+    public K getKey() {
+        return Key;
+    }
+
+    public void setKey(K key) {
+        this.Key=key;
+    }
+
+    public INode getNext() {
+        return next;
+    }
+
+    public void setNext(INode next) {
+        this.next = next;
+    }
+}
